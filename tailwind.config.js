6778c85a64ff4756f5cbfc85f52718ff3a4273bc
Your Name <mohammadhg98@gmail.com>
2025-03-07 13:24:49 -0800

    tailwind configure

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// filepath: c:\Users\Mohammad\Documents\Sources\movie-show-app\movie-app\tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
